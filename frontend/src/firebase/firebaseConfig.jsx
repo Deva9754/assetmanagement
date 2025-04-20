1e71951a2ce7374a50471dd158e88331439e32f2
Deva9754 <deva.sahu2001@gmail.com>
2025-04-20 20:37:24 +0530

    feat:firebase added

---
+++ frontend/src/firebase/firebaseConfig.jsx
@@ -0,0 +1,16 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZrCJ_DOarn3n7GazoKzAH8rhNJk8gtR4",
+  authDomain: "assetmanagement-6cc97.firebaseapp.com",
+  projectId: "assetmanagement-6cc97",
+  storageBucket: "assetmanagement-6cc97.firebasestorage.app",
+  messagingSenderId: "467048722925",
+  appId: "1:467048722925:web:1a0be66fd81b48f10177e4",
+  measurementId: "G-LZE03RYZJ0"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);

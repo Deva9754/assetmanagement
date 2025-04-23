f95c783960453f0a74f01f92d36c7769e1888b3b
Deva9754 <deva.sahu2001@gmail.com>
2025-04-23 13:24:10 +0530

    create custom hook

---
+++ frontend/src/hooks/useUserRole.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { auth, db } from "../firebase/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+
+const useUserRole = () => {
+    const [role, setRole] = useState("loading");
+  
+    useEffect(() => {
+      const fetchUserRole = async () => {
+        const user = auth.currentUser;
+        if (user) {
+          try {
+            const userDoc = await getDoc(doc(db, "users", user.uid));
+            if (userDoc.exists()) {
+              const userData = userDoc.data();
+              setRole(userData.role); // "admin" or "user"
+            } else {
+              setRole("none"); // No doc found
+            }
+          } catch (error) {
+            console.error("Error fetching user role:", error);
+            setRole("error");
+          }
+        } else {
+          setRole("none"); // No user logged in
+        }
+      };
+  
+      fetchUserRole();
+    }, []);
+  
+    return role;
+  };
+  
+
+  export default useUserRole;

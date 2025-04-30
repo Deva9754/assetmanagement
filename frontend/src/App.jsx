df4b1c46d7e29c79604f73af3a12b6cb533f1bdf
Deva9754 <deva.sahu2001@gmail.com>
2025-04-30 13:19:40 +0530

    feat: route changed path of reports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,8 @@ import Reports from "./pages/Reports";
 import UserTransaction from "./pages/UserTransaction";
 import Layout from "./components/Layout";
 
+
+
 const ProtectedRoute = ({ children }) => {
   const { currentUser } = useAuth();
   return currentUser ? children : <Navigate to="/" />;
